08dbf7ea71995390329ecb34dac61e2858206c80
hai046 <haizhu12345@gmail.com>
2022-01-08 10:50:53 +0800

    udpate

--- appDownloads/utils/alert.py
+++ appDownloads/utils/alert.py
@@ -4,26 +4,19 @@ import requests
 
 
 class Alert:
-    def __init__(this, groupName):
-        this.groupName = groupName
+    def __init__(this, key):
+        this.key = key
 
     def send(self, title, download_url, msg):
-        # content = {
-        #     "msgtype": "markdown",
-        #     "markdown": {
-        #         "content": title +
-        #                    "\n## [下载地址 " + download_url + "]( " + download_url + ")" +
-        #                    "\n" + msg
-        #     }
-        # }
-
         content = {
-            "title": (title if self.groupName == '' else self.groupName),
-            "users": ["denghaizhu", "zhouweisong", "zhangyahao"],
-            "content": '下载地址：%s\n%s' % (download_url, msg),
-            "type": "group"
+            "msgtype": "markdown",
+            "markdown": {
+                "content": title +
+                           "\n## [下载地址 " + download_url + "]( " + download_url + ")" +
+                           "\n" + msg
+            }
         }
-        requests.post('https://alert.shouyouwan.net/feishu/send', json=content)
+        requests.post('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=%s' % self.key, json=content)
 
 
 if __name__ == '__main__':
